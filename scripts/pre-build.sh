1bc493d3aff2e6304b9312aa8b9ccc25e7fa3bfb
Mohamed Jeylani <95252871+mjeylanii@users.noreply.github.com>
2024-05-26 15:53:51 -0400

    chore: Decode AMPLIFY_CONFIG from base64 and create Amplify config json file

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -3,11 +3,9 @@
 # Load environment variables
 source .env
 
-# Create a JSON string from the AMPLIFY_CONFIG environment variable
-json_string="{ \"AMPLIFY_CONFIG\": \"$AMPLIFY_CONFIG\" }"
+# Decode the AMPLIFY_CONFIG environment variable from base64 to JSON
+json_decoded=$(echo -n "$AMPLIFY_CONFIG" | base64 --decode)
 
-# Convert the JSON string to base64
-base64_string=$(echo -n "$json_string" | base64)
 
-# Write the base64 string to a file
-echo "$base64_string" > "src/configs/amplify/amplify-$FLAVOR.json"
+# Write the decoded JSON string to a file
+echo "$json_decoded" > "../src/configs/amplify/amplify-$FLAVOR.json"
