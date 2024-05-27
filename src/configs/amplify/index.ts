86a3fb6f943ca4ee181f4247833e2c23f7b1b1fd
Mohamed Jeylani <95252871+mjeylanii@users.noreply.github.com>
2024-05-27 10:38:51 -0400

    refactor how the amplify configs is generated and read

--- src/configs/amplify/index.ts
+++ src/configs/amplify/index.ts
@@ -1,22 +1,16 @@
 import type { ResourcesConfig } from "aws-amplify"
 
-import { flavor } from "../envs.config"
 //@ts-ignore
-import * as AmplifyDev from "./amplify-dev.json"
-//@ts-ignore
-import * as AmplifyProd from "./amplify-production.json"
-//@ts-ignore
-import * as AmplifyStaging from "./amplify-staging.json"
+import * as AmplifyConfig from "../../../amplify.json"
 
 const getAmplifyConfig = (): ResourcesConfig => {
-	switch (flavor) {
-		case "production":
-			return AmplifyProd as ResourcesConfig
-		case "staging":
-			return AmplifyStaging as ResourcesConfig
-		case "dev":
-			return AmplifyDev as ResourcesConfig
+	const config = AmplifyConfig
+
+	if (!config) {
+		throw new Error(`Amplify config not found`)
 	}
+
+	return config
 }
 
 export default getAmplifyConfig
